c3c312e656e637c25b7cf962293d739cf26f6ecc
Sollimann <kristoffer.solberg@cognite.com>
2022-06-21 00:14:07 +0200

    added 3d libraries in examples crate

--- examples/src/3d.rs
+++ examples/src/3d.rs
@@ -1,3 +1,19 @@
+use kiss3d::light::Light;
+use kiss3d::window::Window;
+use na::{UnitQuaternion, Vector3};
+use nalgebra as na;
+
 fn main() {
-    println!("placeholder")
+    let mut window = Window::new("Kiss3d: cube");
+    let mut c = window.add_cube(1.0, 1.0, 1.0);
+
+    c.set_color(1.0, 0.0, 0.0);
+
+    window.set_light(Light::StickToCamera);
+
+    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+
+    while window.render() {
+        c.prepend_to_local_rotation(&rot);
+    }
 }
