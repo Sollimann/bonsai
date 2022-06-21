6086f05e8285d72a6c269da98a5d165c1e852710
Sollimann <kristoffer.solberg@cognite.com>
2022-06-21 22:35:28 +0200

    added timer

--- bonsai/src/event.rs
+++ bonsai/src/event.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code, unused_variables)]
 /// Used to identify events arguments provided by traits.
 ///
 /// Use format `<api>/<event>` to avoid naming collision.
@@ -66,9 +67,39 @@ impl UpdateEvent for Event {
     }
 }
 
+use std::time::Instant;
+
+#[derive(Debug, Clone)]
+pub struct Timer {
+    start: Instant,
+    now: Instant,
+}
+
+impl Timer {
+    pub fn init_time() -> Timer {
+        let init = Instant::now();
+        Timer { start: init, now: init }
+    }
+
+    pub fn duration_since_start(&self) -> f64 {
+        let new_now: Instant = Instant::now();
+        let duration = new_now.duration_since(self.start);
+        duration.as_secs_f64()
+    }
+
+    pub fn get_dt(&mut self) -> f64 {
+        let new_now: Instant = Instant::now();
+        let duration = new_now.duration_since(self.now);
+        self.now = new_now;
+        duration.as_secs_f64()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::thread::sleep;
+    use std::time::Duration;
 
     #[test]
     fn test_update_args() {
@@ -77,5 +108,26 @@ mod tests {
 
         let e: Event = UpdateArgs { dt: 0.0 }.into();
         let _: Option<Event> = UpdateEvent::from_update_args(&UpdateArgs { dt: 1.0 }, &e);
+    }
+
+    #[test]
+    fn test_timer() {
+        let mut timer = Timer::init_time();
+        sleep(Duration::new(0, 0.1e+9 as u32));
+        let duration = timer.duration_since_start();
+        let dt = timer.get_dt();
+
+        assert!(duration < 1.0);
+        assert!(dt < 0.2);
+        assert!(dt >= 0.1);
+
+        sleep(Duration::new(0, 0.3e+9 as u32));
+        let duration = timer.duration_since_start();
+        let dt = timer.get_dt();
+
+        assert!(duration < 1.0);
+        assert!(duration > 0.3);
+        assert!(dt > 0.2);
+        assert!(dt < 0.4);
     }
 }
--- examples/src/3d.rs
+++ examples/src/3d.rs
@@ -1,19 +1,87 @@
-use kiss3d::light::Light;
+#![allow(dead_code, unused_variables, unused_imports)]
+use bonsai::{Event, State, Status::Failure, Status::Success, UpdateArgs};
 use kiss3d::window::Window;
+use kiss3d::{light::Light, scene::SceneNode};
 use na::{UnitQuaternion, Vector3};
 use nalgebra as na;
+
+/// Underlying numeric type.
+pub type Scalar = f32;
+
+/// Animations supported by Sprite
+#[derive(Clone, PartialEq)]
+pub enum Animation {
+    /// duration, x, y
+    ///
+    /// Move to specified position
+    MoveTo(f64, Scalar, Scalar),
+    /// duration, x, y
+    ///
+    /// Move to specified position, relatively
+    MoveBy(f64, Scalar, Scalar),
+    /// duration, deg
+    ///
+    /// Rotate to specified degree
+    RotateTo(f64, Scalar),
+    /// duration, deg
+    ///
+    /// Rotate to specified degree, relatively
+    RotateBy(f64, Scalar),
+    /// duration, sx, sy
+    ///
+    /// Scale to specified scale
+    ScaleTo(f64, Scalar, Scalar),
+    /// duration, sx, sy
+    ///
+    /// Scale to specified scale, relatively
+    ScaleBy(f64, Scalar, Scalar),
+    /// duration, times
+    Blink(f64, usize),
+    /// duration, r, g, b
+    ChangeColor(f64, Scalar, Scalar, Scalar),
+}
+
+// A test state machine that can increment and decrement.
+fn tick(_c: &SceneNode, dt: f64, state: &mut State<Animation, ()>) {
+    let e: Event = UpdateArgs { dt }.into();
+
+    state.event(&e, &mut |args| match *args.action {
+        Animation::MoveTo(duration, x, y) => {
+            todo!()
+        }
+        Animation::MoveBy(duration, x, y) => {
+            todo!()
+        }
+        Animation::RotateTo(duration, rad) => {
+            todo!()
+        }
+        Animation::RotateBy(duration, rad) => {
+            todo!()
+        }
+        Animation::ScaleTo(duration, sx, sy) => {
+            todo!()
+        }
+        Animation::ScaleBy(duration, sx, sy) => {
+            todo!()
+        }
+        Animation::Blink(duration, times) => todo!(),
+        Animation::ChangeColor(duration, r, g, b) => todo!(),
+    });
+
+    // c
+}
 
 fn main() {
     let mut window = Window::new("Kiss3d: cube");
     let mut c = window.add_cube(1.0, 1.0, 1.0);
 
     c.set_color(1.0, 0.0, 0.0);
-
     window.set_light(Light::StickToCamera);
 
     let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
 
     while window.render() {
+        // tick()
         c.prepend_to_local_rotation(&rot);
     }
 }
