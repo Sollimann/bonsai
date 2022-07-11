4dda4b69d24c78463c0b418ac7235de05f80da0b
Sollimann <kristoffer.solberg@cognite.com>
2022-07-11 02:15:40 +0200

    started on rust code example for readme

--- examples/src/boids_swarm.rs
+++ examples/src/boids_swarm.rs
@@ -1,33 +1,3 @@
-<h1 align="center" style="font-family:Papyrus; font-size:4em;"> Bonsai 盆栽 </h1>
-<p align="center">
-  <img src="https://github.com/Sollimann/bonsai/blob/main/docs/resources/gifs/bonsai.gif" width="350" ">
-</p>
-
-<p align="center">
-    <em>Rust implementation of Behavior Trees</em>
-</p>
-
-<!-- [![codecov](https://codecov.io/gh/Sollimann/CleanIt/branch/main/graph/badge.svg?token=EY3JRZN71M)](https://codecov.io/gh/Sollimann/CleanIt) -->
-<!-- [![version](https://img.shields.io/badge/version-1.0.0-blue)](https://GitHub.com/Sollimann/CleanIt/releases/) -->
-[![Build Status](https://github.com/Sollimann/bonsai/workflows/rust-ci/badge.svg)](https://github.com/Sollimann/bonsai/actions)
-[![minimum rustc 1.60](https://img.shields.io/badge/rustc-1.60+-blue.svg)](https://rust-lang.github.io/rfcs/2495-min-rust-version.html)
-[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Sollimann/bonsai/graphs/commit-activity)
-[![GitHub pull-requests](https://img.shields.io/github/issues-pr/Sollimann/bonsai.svg)](https://GitHub.com/Sollimann/bonsai/pulls)
-[![GitHub pull-requests closed](https://img.shields.io/github/issues-pr-closed/Sollimann/bonsai.svg)](https://GitHub.com/Sollimann/bonsai/pulls)
-![ViewCount](https://views.whatilearened.today/views/github/Sollimann/bonsai.svg)
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
-
-
-## What is a Behavior Tree?
-
-A _Behavior Tree_ (BT) is a data structure in which we can set the rules of how certain _behavior's_ can occur, and the order in which they would execute. BTs are a very efficient way of creating complex systems that are both modular and reactive. These properties are crucial in many applications, which has led to the spread of BT from computer game programming to many branches of AI and Robotics.
-
-<p align="center">
-  <img src="https://github.com/Sollimann/bonsai/blob/readme-example/docs/resources/images/npc_bt.png" width="600" ">
-</p>
-
-```rust
 use std::{collections::HashMap, thread::sleep, time::Duration};
 
 use bonsai::{
@@ -104,10 +74,3 @@ fn main() {
         game_tick(&mut timer, &mut bt);
     }
 }
-```
-
-## Contents
-
-* [Concepts](docs/concepts/README.md)
-* [Development Guide](DEVELOPMENT.md)
-* [Kanban Board](https://github.com/Sollimann/b3/projects/1)
