24249b6aec23d75ef92d2230d9ca11d4c0a24bd4
kaphula <vaaka@protonmail.com>
2023-12-13 21:26:14 +0200

    Remove useless comment.

--- bonsai/tests/dynamic_behavior_tests.rs
+++ bonsai/tests/dynamic_behavior_tests.rs
@@ -15,7 +15,6 @@ enum TestActions {
 fn tick(mut acc: usize, dt: f64, t: &mut f64, counter: &mut usize, state: &mut State<TestActions>) -> usize {
     let e: Event = UpdateArgs { dt }.into();
 
-    // let (_s, _t) = state.tick(&e, &mut |args| match args.action {
     let (_s, _t) = state.tick(
         &e,
         &mut (),
