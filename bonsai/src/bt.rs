829798ad5cc8585cadc57432dfaf158f099a1e2d
kaphula <vaaka@protonmail.com>
2023-12-14 15:49:01 +0200

    Copy tick function documentation.

--- bonsai/src/bt.rs
+++ bonsai/src/bt.rs
@@ -57,6 +57,18 @@ impl<A: Clone + Debug, K: Debug> BT<A, K> {
         }
     }
 
+    /// Updates the cursor that tracks an event.
+    ///
+    /// The action need to return status and remaining delta time.
+    /// Returns status and the remaining delta time.
+    ///
+    /// Passes event, delta time in seconds, action and state to closure.
+    /// The closure should return a status and remaining delta time.
+    ///
+    /// return: (Status, f64)
+    /// function returns the result of the tree traversal, and how long
+    /// it actually took to complete the traversal and propagate the
+    /// results back up to the root node
     #[inline]
     pub fn tick<E, F>(&mut self, e: &E, f: &mut F) -> (Status, f64)
     where
